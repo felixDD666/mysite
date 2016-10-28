6ac6126eace48801a4aec53c1816d2ed7f2f1605
felixDD666 <felixdurandom@gmail.com>
2016-10-28 17:53:26 +0200

    Git con email corregido

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,8 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '951753.durex@gmail.com'
-EMAIL_HOST_PASSWORD = 'Copazo951753+'
+EMAIL_HOST_USER = 'podoAlvaro@gmail.com'
+EMAIL_HOST_PASSWORD = '951753+-'
 EMAIL_PORT = 587
 
 
