8abbb9c15301d898c369b8a4b052d0ea111995ff
felixDD666 <felixdurandom@gmail.com>
2016-11-16 13:27:09 +0100

    16/11 v5

--- polls/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    nombre = forms.CharField(required=True,widget=forms.Textarea)
-    email = forms.EmailField(required=True)
-    telefono = forms.CharField(required=True,widget=forms.Textarea)
-    mensaje = forms.CharField(required=True,widget=forms.Textarea)
